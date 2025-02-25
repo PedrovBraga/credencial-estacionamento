32bd0200e0a5205bd836946f802cc183dc41c69f
PedrovBraga <bragavpedro@hotmail.com>
2025-02-25 13:27:59 -0300

    Ajuste nos retornos e algumas tratativas

--- sistema/Controlador/PessoaControlador.php
+++ sistema/Controlador/PessoaControlador.php
@@ -55,21 +55,25 @@ class PessoaControlador extends Controlador {
             $representante = new Representante();
             // Confere check PNE (se deficiente)
             if(isset($dados_separados['outro']['DEF'])){
+                $doc_representante = $dados_separados['representante']['RG'] ?? $dados_separados['representante']['CPF'];
+                $representante = $representante->buscaPorCPFouRG($doc_representante) ?? $representante;
+                // Preenche representante com os dados do formulário, se já existir, sobrescreve oq for ser atualizado
                 $representante->preencher($dados_separados['representante']);
             }
 
             try {
+                // Atualiza representante e beneficiário
                 if(isset($dados_separados['outro']['DEF'])){
+                    $representante->DATA_ATUALIZACAO = (new DateTime())->format('Y-m-d H:i:s');
                     $representante->gravar();
-                    // echo print_r($representante);
                     $beneficiario->gravar($representante);
                 } else {
                     $beneficiario->gravar();
                 }
 
-                $json = ['status' => 1, 'mensagem' => 'Usuário atualizado!', 'urlConfirmar' => URL_DESENVOLVIMENTO . '/pessoa/editar'];
+                $json = ['status' => 1, 'mensagem' => 'Beneficiário atualizado!', 'urlConfirmar' => URL_DESENVOLVIMENTO . '/pessoa/editar'];
             } catch (Exception $e) {
-                $json = ['status' => 0, 'mensagem' => 'Falha ao atualizar usuário! '.$e->getMessage()];
+                $json = ['status' => 0, 'mensagem' => 'Falha ao atualizar Beneficiário! '.$e->getMessage()];
             }
 
             header('Content-Type: application/json; charset=utf-8');
