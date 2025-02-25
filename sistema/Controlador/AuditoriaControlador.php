5329793ee9510b7a8e7c692d940a1e22929bfc5e
PedrovBraga <bragavpedro@hotmail.com>
2025-02-25 13:07:14 -0300

    Adicionei as tratativas da auditoria, regras, consultas e gravar

--- sistema/Controlador/AuditoriaControlador.php
+++ sistema/Controlador/AuditoriaControlador.php
@@ -1,6 +1,8 @@
 <?php
 namespace Sistema\Controlador;
 
+use sistema\Modelo\Auditoria;
+use Sistema\Modelo\Usuario;
 use sistema\Nucleo\Controlador;
 
 class AuditoriaControlador extends Controlador {
@@ -10,12 +12,44 @@ class AuditoriaControlador extends Controlador {
     
     public function consultar(): void {
         $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-        
-        if(isset($dados)){
-            // Consulta registros do funcionário e envia dados pra view
-        } 
 
-        echo $this->template->renderizar('auditoria/auditoria.html', []);
+        $usuarios = (new Usuario())->listar();
+
+        echo $this->template->renderizar('auditoria/auditoria.html', [
+            'usuarios' => $usuarios
+        ]);
+    }
+    
+    public function consultarMunicipe(): void {
+        
+        $num_doc = filter_input(INPUT_GET, 'doc');
+
+        $auditoria_registros = (new Auditoria())->buscaPorCPFOuRG($num_doc); 
+
+        // var_dump($auditoria_registros);
+
+        $auditoriasArray = array_map(function($auditoria) {
+            return (array) $auditoria; // Convertendo cada credencial em um array
+        }, $auditoria_registros);
+
+        $json = ['status' => 1, 'mensagem' => 'Beneficiário encontrado!', 'auditorias' => (array) $auditoriasArray];
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($json);
+    }
+    
+    public function consultarOperador(): void {
+        $operador = filter_input(INPUT_GET, 'operador');
+
+        $auditoria_registros = (new Auditoria())->buscaPorOperador($operador);
+
+        $auditoriaArray = array_map(function($auditoria) {
+            return (array) $auditoria; // Convertendo cada credencial em um array
+        }, $auditoria_registros);
+
+
+        $json = ['status' => 1, 'mensagem' => 'Operador encontrado!', 'auditorias' => $auditoriaArray];
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($json);
     }
 }
 
