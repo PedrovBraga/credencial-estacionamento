a65e97025ef95ee174e0821b17b3d2d04b9c1e4a
PedrovBraga <bragavpedro@hotmail.com>
2025-04-02 09:23:39 -0300

    Delete de arquivos não utilizados

--- inc/footer.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-
-    <div class="footer text-center">
-        © <?php echo date('Y') ?> Prefeitura Municipal de Jacareí - Diretoria de TI
-    </div>
-
-</html>
--- inc/verifica_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!isset($_SESSION['id_login'])) {
-    echo "<script>
-        window.location.href = '../login.php'
-        </script>";
-    exit;
-}
-?>
